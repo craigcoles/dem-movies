52719b4b021f73a504ddc0538fe2ae611796734b
Craig Coles <craig.coles@truedigital.co.uk>
2015-02-26 13:38:44 +0000

    Dem views

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express')
 var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  // response.send('Hello World!')
+  response.render('pages/home')
 })
 
 app.listen(app.get('port'), function() {
