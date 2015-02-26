c40c0ee7a13ec8c6bd1bf28c0fe04f1f8136e5f8
Kev Rowe <kevin.rowe@truedigital.co.uk>
2015-02-26 13:53:00 +0000

    Config shizzle

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var express = require('express')
-var app = express();
+var config = require('./config'),
+	express = require('express'),
+	app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  // response.send('Hello World!')
-  response.render('pages/home')
-})
+  //response.send(config.API_KEY)
+  response.render('pages/home');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
