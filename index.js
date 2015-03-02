474ad05c758bcbbff71be7e3e50176701a486c5a
Craig Coles <craig.coles@truedigital.co.uk>
2015-03-02 09:59:30 +0000

    Added popular page with links

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/movie', movie.info);
+app.get('/popular', movie.list);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
